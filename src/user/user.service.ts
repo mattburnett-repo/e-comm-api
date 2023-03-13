dec5ab3a7466d7b0f579e92ed13ac1ea6079416d
Matt Burnett <mburnett@tutanota.de>
2023-03-13 19:57:46 +0100

    fix: added relations to user.service.ts and e2e test

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -30,17 +30,177 @@ export class UserService {
 
   findAll(): Promise<User[]> {
     this.logger.log(`UserService findAll`)
-    return this.userRepository.find()
+    return this.userRepository.find({
+      relations: ['address', 'cart', 'cart.cartItem', 'order', 'payment'],
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        password: true,
+        firstName: true,
+        lastName: true,
+        refreshToken: true,
+        address: {
+          id: true,
+          user_id: true,
+          firstName: true,
+          lastName: true,
+          address_1: true,
+          address_2: true,
+          city: true,
+          postalCode: true,
+          country: true
+        },
+        cart: {
+          id: true,
+          user_id: true,
+          name: true,
+          description: true,
+          cartItem: {
+            id: true,
+            product_id: true,
+            productName: true,
+            productQuantity: true,
+            productPrice: true,
+            lineItemTotalPrice: true
+          }
+        },
+        order: {
+          id: true,
+          cart_id: true,
+          user_id: true,
+          order_date: true,
+          total_price: true,
+          payment_id: true
+        },
+        payment: {
+          id: true,
+          user_id: true,
+          payment_type_id: true,
+          transaction_status: true,
+          amount: true
+        }
+      }
+    })
   }
 
   findOneById(id: string): Promise<User> {
     this.logger.log(`UserService findById: ${id}`)
-    return this.userRepository.findOne({ where: { id } })
+
+    return this.userRepository.findOne({
+      where: {
+        id: id
+      },
+      relations: ['address', 'cart', 'cart.cartItem', 'order', 'payment'],
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        password: true,
+        firstName: true,
+        lastName: true,
+        refreshToken: true,
+        address: {
+          id: true,
+          user_id: true,
+          firstName: true,
+          lastName: true,
+          address_1: true,
+          address_2: true,
+          city: true,
+          postalCode: true,
+          country: true
+        },
+        cart: {
+          id: true,
+          user_id: true,
+          name: true,
+          description: true,
+          cartItem: {
+            id: true,
+            product_id: true,
+            productName: true,
+            productQuantity: true,
+            productPrice: true,
+            lineItemTotalPrice: true
+          }
+        },
+        order: {
+          id: true,
+          cart_id: true,
+          user_id: true,
+          order_date: true,
+          total_price: true,
+          payment_id: true
+        },
+        payment: {
+          id: true,
+          user_id: true,
+          payment_type_id: true,
+          transaction_status: true,
+          amount: true
+        }
+      }
+    })
   }
 
   findOneByUsername(username: string): Promise<User> {
     this.logger.log(`UserService findByUsername: ${username}`)
-    return this.userRepository.findOne({ where: { username } })
+    return this.userRepository.findOne({
+      where: {
+        username
+      },
+      relations: ['address', 'cart', 'cart.cartItem', 'order', 'payment'],
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        password: true,
+        firstName: true,
+        lastName: true,
+        refreshToken: true,
+        address: {
+          id: true,
+          user_id: true,
+          firstName: true,
+          lastName: true,
+          address_1: true,
+          address_2: true,
+          city: true,
+          postalCode: true,
+          country: true
+        },
+        cart: {
+          id: true,
+          user_id: true,
+          name: true,
+          description: true,
+          cartItem: {
+            id: true,
+            product_id: true,
+            productName: true,
+            productQuantity: true,
+            productPrice: true,
+            lineItemTotalPrice: true
+          }
+        },
+        order: {
+          id: true,
+          cart_id: true,
+          user_id: true,
+          order_date: true,
+          total_price: true,
+          payment_id: true
+        },
+        payment: {
+          id: true,
+          user_id: true,
+          payment_type_id: true,
+          transaction_status: true,
+          amount: true
+        }
+      }
+    })
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
